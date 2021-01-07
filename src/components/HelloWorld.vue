8ec39d23eb9b60423e7a9fc0bd6127ecde7adc2d
LAPTOP-F2U3HPDF\18618 <18618404236@163.com>
2021-01-08 00:40:53 +0800

    ts无法直接引用json

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,9 +9,9 @@
 
 <script lang="ts">
 import {ref,reactive,watchEffect,onMounted} from 'vue'
-import style from '../assets/css/style.modules.css'
+import style from '../assets/css/style.modules.css' 
 import menu from '../method/menu'
-import menuArr from '../assets/data/menu.json'
+import menuData from '../assets/data/menu'
 
 export default {
   props:{
@@ -22,11 +22,11 @@ export default {
       id :number;
       name :string;
       content :string;
-      icon :string;
+      icon ?:string;
       children ?:IMEMU[];
     }
 
-    let arr: IMEMU[]= menuArr
+    let arr: IMEMU[]= menuData.menuArr
     const text: string = props.msg
     enum test{
       st = '你好',

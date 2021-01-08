7642d8ad6cd2c33636e8d8fb0a0cb610f2fb0d0b
ils <ly@bjtu.edu.cn>
2021-01-08 17:34:58 +0800

    类

---
+++ src/method/game.ts
@@ -0,0 +1,18 @@
+class person{
+
+}
+class fammer extends person{
+
+}
+class engineer extends person{
+
+}
+class banker extends person{
+
+}
+export default{
+    person,
+    fammer,
+    engineer,
+    banker
+}

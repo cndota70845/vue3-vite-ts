4025652a8405a95ea5b661e06ce7a51cbb7f812d
LAPTOP-F2U3HPDF\18618 <18618404236@163.com>
2021-02-26 00:30:20 +0800

    +

--- src/plugin/danmu/danmu.vue
+++ src/plugin/danmu/danmu.vue
@@ -5,9 +5,9 @@
             <video src="" id="J_danmuVideo" controls></video>
         </div>
         <div class="tool-box">
-            <a-input></a-input>
-            <a-button></a-button>
-            <a-input></a-input>
+            <a-input class="text"></a-input>
+            <a-input type="color" class="color"></a-input>
+            <a-button class="btn" type="primary">发送弹幕</a-button>
         </div>
     </div>
 </template>
@@ -19,5 +19,34 @@ export default {
 </script>
 
 <style>
-
+    .danmu{
+        width: 1000px;
+        margin: 0 auto;
+    }
+    .video-wrapper{
+        position: relative;
+    }
+    .video-wrapper video{
+        width: 100%;
+    }
+    .video-wrapper canvas{
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+    .text{
+        width: 300px;
+        margin-right: 20px;
+    }
+    .color{
+        height: 30px;
+        width: 50px;
+        margin-right: 20px;
+    }
+    .tool-box{
+        margin-top: 20px;
+        text-align: left;
+    }
 </style>

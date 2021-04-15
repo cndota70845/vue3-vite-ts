80aab34e4099001b77b00a240cbcbec7797a1512
LAPTOP-F2U3HPDF\18618 <18618404236@163.com>
2021-04-15 23:22:17 +0800

    ++

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import App from './App.vue';
 import './index.css';
 import 'ant-design-vue/dist/antd.css';
 import router from './router';
-import {Menu,Layout,message,Input,Button,Row,Col,Table} from 'ant-design-vue';
+import antd from 'ant-design-vue';
+import {message} from 'ant-design-vue';
 import api from './request/api/index';
 import cookies from "cookies-js";
 import { SelectorUI } from './plugin/LisUI/index.js';
 
 const app = createApp(App as object);
-app.use(router).use(Menu).use(Layout).use(Input).use(Button).use(Row).use(Col).use(Table).use(SelectorUI);
+app.use(router).use(antd).use(SelectorUI);
 app.mount('#app');
 app.config.globalProperties.$router = router;
 app.config.globalProperties.$message = message;
--- src/views/practice/leetcode.vue
+++ src/views/practice/leetcode.vue
@@ -1,131 +1,68 @@
 <template>
     <div>
         <p>
-            输入：{{l1}},{{l2}}
+            输入：<a-input-number v-model:value="n" :min="0"></a-input-number>
         </p>
         <p>
-           输出：{{addTwoNumbers(l1,l2)}} 
+           输出：{{nthUglyNumber(n)}} 
         </p>
     </div>
 </template>
 
 <script>
+import {ref} from 'vue';
 export default {
     name: 'LeetCode',
     setup () {
-        // var nums = [3,2,4], target = 6;
 
-        // var twoSum = function(nums, target) {
-        //     for (let i = 0; i < nums.length; i++) {
-        //         for (let k = nums.length -1; k > i; k--) {
-        //             if (nums[i] + nums[k] == target) {
-        //                 return [i,k];
-        //             }
-        //         }
-        //     }
-        // }
+        var n = ref(12);
 
-        // var twoSum = function(nums, target) {
-        //     var map = new Map;
-        //     nums.forEach((element,index) => {
-        //         map.set(element,index);
-        //     });
-        //     for (let i = 0; i < nums.length; i ++) {
-        //         if (map.has(target - nums[i])) {
-        //             let index = map.get(target - nums[i]);
-        //             if (index != i) {
-        //                 return [i,index];
-        //             }
-        //         }
-        //     };
-        // }
+        var nthUglyNumber = function(n) {
 
-        function ListNode(val, next) {
-            this.val = (val===undefined ? 0 : val)
-            this.next = (next===undefined ? null : next)
-        }
+            function UglyNumber (target) {
+                var arr = [2,3,5];
+                var res = false;
+                if (target > 0) {
+                    if (target === 1) {
+                        res = true;
+                    }
+                    else {
+                        for (let i = 0; i < arr.length; i++){
+                            let yu = target % arr[i];
+                            if (yu === 0) {
+                                let num = parseInt(target/arr[i]);
+                                if (arr.includes(num)) {
+                                    res = true;
+                                    return res;
+                                }
+                                else {
+                                    res = UglyNumber (num);
+                                }
+                            }
+                        }
+                    }
 
-        function reverseArr (target,index) {
-            if (index < target.length) {
-                return {
-                    val:target[index++],
-                    next:index==target.length?null:reverseArr(target,index)
                 }
+                return res;
             }
-        }
-
-        // var l1 = new ListNode(2,new ListNode(4,new ListNode(3))), l2 = new ListNode(5,new ListNode(6,new ListNode(4)));
-        // var l1 = new ListNode(0), l2 = new ListNode(0);
-        var index = 0;
-        var arr = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1];
-        var l1 = reverseArr(arr,index), 
-            l2 = new ListNode(5,new ListNode(6,new ListNode(4,new ListNode(9))));
-
-        var addTwoNumbers = function(l1, l2) {
-            // var arr1 = [];
-            // var arr2 = [];
-            // function transArr (target,response) {
-            //     if (target.val || target.val===0) {
-            //         response.push(target.val);
-            //         if (target.next === null) {
-            //             return;
-            //         }
-            //         else{
-            //             transArr(target.next,response);
-            //         }
-            //     }
-            // }
-
-            // function reverseArr (target,index) {
-            //     if (index < target.length) {
-            //         return {
-            //             val:target[index++],
-            //             next:index==target.length?null:reverseArr(target,index)
-            //         }
-            //     }
-            // }
-
-            // transArr(l1,arr1);
-            // transArr(l2,arr2);
-            // arr1.reverse();
-            // arr2.reverse();
-            // var arr3 = String(parseInt((arr1.toString()).replace(new RegExp(",","gm"),'')) + parseInt((arr2.toString()).replace(new RegExp(",","gm"),'')));
-            // console.log(Number((arr1.toString()).replace(new RegExp(",","gm"),'')),arr2,arr3);
-            // var l3 = arr3.split('').map(Number);
-            // l3.reverse();
-            // var index = 0;
-            // return reverseArr(l3,index);
-
-            let head = null, tail = null;
-            let carry = 0;
-            while (l1 || l2) {
-                const n1 = l1 ? l1.val : 0;
-                const n2 = l2 ? l2.val : 0;
-                const sum = n1 + n2 + carry;
-                if (!head) {
-                    head = tail = new ListNode(sum % 10);
-                } else {
-                    tail.next = new ListNode(sum % 10);
-                    tail = tail.next;
+            if (n > 1) {
+                var arr = [1];
+                var i = 2;
+                for (let i = 2; arr.length <= n; i++) {
+                    if (UglyNumber(i)) {
+                        arr.push(i);
+                    }
                 }
-                carry = Math.floor(sum / 10);
-                if (l1) {
-                    l1 = l1.next;
-                }
-                if (l2) {
-                    l2 = l2.next;
-                }
+                return arr[arr.length-2];
             }
-            if (carry > 0) {
-                tail.next = new ListNode(carry);
+            else if (n === 1) {
+                return 1;
             }
-            return head;
         };
 
         return {
-            l1,
-            l2,
-            addTwoNumbers
+            n,
+            nthUglyNumber
         }
     }
 }

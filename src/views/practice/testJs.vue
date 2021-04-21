a89c2b7efc9b223c163b1e64aeff2d68ac87c6dd
LAPTOP-F2U3HPDF\18618 <18618404236@163.com>
2021-04-21 23:49:48 +0800

    promise

--- src/views/practice/testJs.vue
+++ src/views/practice/testJs.vue
@@ -8,20 +8,30 @@
 export default {
     name:'testJS',
     setup () {
-        let arr = [99,99,1,1,2,3,4,5,5,6];
-        //Set去重 + sort排序;
-        let arr2 = Array.from(new Set(arr)).sort();
-        //重新返回一个每个元素循环一次回调返回的数组;
-        let arr3 = arr.reduce(function(acc,cur,idx,src) {
-            if(!acc.includes(cur)) {
-                acc.push({
-                    id:idx + 1,
-                    value:cur,
-                });
-            }
-            return acc;
-        },[]);
-        console.log(arr,arr2,arr3);
+        // let arr = [99,99,1,1,2,3,4,5,5,6];
+        // //Set去重 + sort排序;
+        // let arr2 = Array.from(new Set(arr)).sort();
+        // //重新返回一个每个元素循环一次回调返回的数组;
+        // let arr3 = arr.reduce(function(acc,cur,idx,src) {
+        //     if(!acc.includes(cur)) {
+        //         acc.push({
+        //             id:idx + 1,
+        //             value:cur,
+        //         });
+        //     }
+        //     return acc;
+        // },[]);
+        // console.log(arr,arr2,arr3);
+
+        const test = new Promise(function(resolve, reject) {
+            setTimeout(function () {
+                let code = 1;
+                resolve(code);
+            },2000);
+        });
+        test.then((val) => {
+            console.log(`val是:${val}`);
+        });
     }
 }
 </script>
